9e7c0bea48053a386f19b834e74ee5cae06a7cb2
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2024-10-21 17:14:27 +0500

    Create object.cpp

---
+++ object.cpp
@@ -0,0 +1,25 @@
+#include "object.h"
+
+object::object()
+{
+}
+
+object::~object()
+{
+}
+  
+void object::CreateObject()
+{
+}
+
+void object::MovingObject()
+{
+}
+
+void object::GetCoords()
+{
+}
+
+void object::SetCoords()
+{
+}
