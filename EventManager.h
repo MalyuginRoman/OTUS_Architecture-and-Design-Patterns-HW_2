410bc2913f0e7f799a89dded57a4d60bae09816d
MalyuginRoman <75801868+MalyuginRoman@users.noreply.github.com>
2024-10-24 17:19:00 +0500

    Create EventManager.h

---
+++ EventManager.h
@@ -0,0 +1,12 @@
+class EventManager
+{
+public:
+    EventManager();
+    ~EventManager();
+
+    bool init(objectList* list);
+    bool execute(objectList* list);
+
+private:
+    class EventManagerP* imp;
+};
